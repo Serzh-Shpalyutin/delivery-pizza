43d35526a65a1b1265ee975e6853faa0a649991c
Serzh Shpalyutin <serzh.shpalyutin@mail.ru>
2022-04-29 20:54:46 +0300

    added Categories state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-
-import Header from './components/Header'
+import { Header, Categories } from "./components";
 
 function App() {
   return (
     <div className="wrapper">
-      <Header/>
+      <Header />
       <div className="content">
         <div className="container">
           <div className="content__top">
-            <div className="categories">
-              <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
-              </ul>
-            </div>
+            <Categories
+              items={[
+                "Мясные",
+                "Вегетарианские",
+                "Гриль",
+                "Острые",
+                "Закрытые",
+              ]}
+            />
             <div className="sort">
               <div className="sort__label">
                 <svg
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,12 +3,13 @@ import classNames from "classnames";
 
 export default function Button({ onClick, className, outline, children }) {
   return (
-    <button 
-    onClick={onClick}
-    className={classNames('button', className, {
-      'button--outline': outline,
-    })}>
-    {children}
+    <button
+      onClick={onClick}
+      className={classNames("button", className, {
+        "button--outline": outline,
+      })}
+    >
+      {children}
     </button>
   );
 }
