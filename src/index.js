ddadf2d4122e5724673df2883894a9d3eeca61be
Serzh Shpalyutin <serzh.shpalyutin@mail.ru>
2022-05-23 22:08:29 +0300

    install redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { createStore } from 'redux';
+
 import "./scss/app.scss";
 import App from "./App";
+
+function counterReducer(state = { value: 0 }, action) {
+  switch (action.type) {
+    case 'counter/incremented':
+      return { value: state.value + 1 }
+    case 'counter/decremented':
+      return { value: state.value - 1 }
+    default:
+      return state
+  }
+}
+
+const store = createStore(counterReducer);
+console.log(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
