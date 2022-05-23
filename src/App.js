7ede3a1408c08e69a5f586946420812b5cb45e73
Serzh Shpalyutin <serzh.shpalyutin@mail.ru>
2022-05-23 18:10:51 +0300

    added json pizzas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
+import axios from "axios";
+
 import { Home, Cart } from "./pages";
 import { Header } from "./components";
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get("http://localhost:3000/db.json").then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+
+    // fetch("http://localhost:3000/db.json")
+    //   .then((resp) => resp.json())
+    //   .then((json) => {
+    //     setPizzas(json.pizzas);
+    //   });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home items={pizzas} />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </div>
