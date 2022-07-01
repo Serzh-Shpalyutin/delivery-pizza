3a5e754a58e54e2fffc652ccd87c1280d92f4470
Serzh Shpalyutin <serzh.shpalyutin@mail.ru>
2022-07-01 19:12:13 +0300

    run cart redux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 import Button from "./Button";
 import logo from "../assets/img/pizza-logo.svg";
 
 export default function Header() {
+
+  const {totalPrice, totalCount} = useSelector(({ cart }) => cart);
+
   return (
     <div className="header">
       <div className="container">
@@ -19,7 +24,7 @@ export default function Header() {
         <div className="header__cart">
           <Link to="/cart">
             <Button className="button--cart">
-              <span>520 ₽</span>
+              <span>{totalPrice}</span>
               <div className="button__delimiter"></div>
               <svg
                 width="18"
@@ -50,7 +55,7 @@ export default function Header() {
                   strokeLinejoin="round"
                 />
               </svg>
-              <span>3</span>
+              <span>{totalCount}</span>
             </Button>
           </Link>
         </div>
---
+++ src/redux/actions/cart.js
@@ -0,0 +1,4 @@
+export const addPizzaToCart = (pizzaObject) => ({
+    type: 'ADD_PIZZA_CART',
+    payload: pizzaObject
+});
